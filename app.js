a3e0ff54a90b240ea75902cae3e7636da1b0eae2
Ashutosh29 <ashutoshsinghnagarkoti@gmail.com>
2021-04-27 11:49:13 +0530

    Added Content

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const alertbox = document.getElementById("alert-box");
 
 
 
-fetch('https://reqres.in/api/users?page=2').then((response) => {
-    return response.json();
+fetch('https://api.github.com/search/users?q=tom').then((response) => {
+    if (response.status === 200) {
+        return response.json();
+    }
+    throw ServerError_msg();
 }).then((Data) => {
-    console.log(Data.data);
-    const Userdata = Data.data;
+    console.log(Data.items);
+    const Userdata = Data.items;
     btn.addEventListener("click", function Search(e) {
         e.preventDefault();
         if (!username.value) {
@@ -18,14 +21,16 @@ fetch('https://reqres.in/api/users?page=2').then((response) => {
             screendata(Userdata);
         }
     });
+}).catch(error => {
+    console.log("Problem on server side");
 })
 
 function screendata(Userdata) {
 
     let Filterdata = Userdata.filter(function (d) {
-        if (d.first_name !== username.value) {
+        if (d.login !== username.value) {
             Notfound_msg();
-        } else if (d.first_name === username.value) {
+        } else if (d.login === username.value) {
             innerbox.classList.add('user-container')
             success_msg();
             return d;
@@ -34,20 +39,28 @@ function screendata(Userdata) {
     Finaldata = Filterdata.map(function (a) {
         return `<div class="user-inner">
            <h2 class="User-head" >USER DETAILS</h2>
-           <h3>First Name:${a.first_name}</h3>
-           <h3>Last Name: ${a.last_name}</h3>
-           <h3>Email Id: ${a.email}</h3>
+           <a href="${a.html_url}"><img src="${a.avatar_url}" class="image"></a>
+           <a href="${a.html_url}"><h3>Name:${a.login}</h3></a>
 
        </div>`;
     })
     const element = document.createElement("div");
     element.innerHTML = Finaldata;
-    if(Finaldata.length){
+    if (Finaldata.length) {
         innerbox.appendChild(element);
     }
-   // innerbox.innerHTML = Finaldata; If we want only one Div to show data of user 
+    // innerbox.innerHTML = Finaldata; If we want only one Div to show data of user 
     username.value = "";
 }
+function ServerError_msg() {
+    alertbox.innerHTML = `<p>*Server Mantenance is Underway*</p>`;
+    alertbox.classList.add('alert-red')
+
+    setTimeout(() => {
+        alertbox.innerHTML = "";
+        alertbox.classList.remove('alert-red');
+    }, 1000)
+};
 function Error_msg() {
     alertbox.innerHTML = `<p>*Cannot leave the User name blank*</p>`;
     alertbox.classList.add('alert-red')
